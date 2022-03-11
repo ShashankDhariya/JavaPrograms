c5c50e8a37770541a9c6d82c3db1d2483b221f8d
dhari <dhari@LAPTOP-GU46GA8D>
2022-03-11 16:00:52 +0530

    Done

--- JavaPrograms/src/Unit_1/FirstProgram.java
+++ JavaPrograms/src/Unit_1/FirstProgram.java
@@ -2,6 +2,18 @@ package Unit_1;
 
 public class FirstProgram {
 	public static void main(String[] args) {
-	System.out.print("Hello");
+	System.out.println("1 Hello World");
+	main(12);
+	main(12.f);
+	main("Hello");
+	}
+	public static void main(int a) {
+		System.out.println("2 Hello World");
+	}
+	public static void main(float b) {
+		System.out.println("3 Hello World");
+	}
+	public static void main(String args) {
+		System.out.println("4 Hello World");
 	}
 }
