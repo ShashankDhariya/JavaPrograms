8e3a226fdc481f492014ec1f03720f1d5be8623b
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-03-14 09:12:46 +0530

    Done

---
+++ JavaPrograms/src/Unit_1/New.java
@@ -0,0 +1,19 @@
+package Unit_1;
+
+public class New {
+		public static void main(String[] args) {
+		System.out.println("1 Hello World");
+		main(12);
+		main(12.f);
+		main("Hello");
+		}
+		public static void main(int a) {
+			System.out.println("2 Hello World");
+		}
+		public static void main(float b) {
+			System.out.println("3 Hello World");
+		}
+		public static void main(String args) {
+			System.out.println("4z Hello World");
+		}
+	}
