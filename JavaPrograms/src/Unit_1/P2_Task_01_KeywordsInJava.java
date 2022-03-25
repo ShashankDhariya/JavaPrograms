352d1b44bdca8ea32abe52e6083f96427d7798bd
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-03-25 16:27:26 +0530

    Done

---
+++ JavaPrograms/src/Unit_1/P2_Task_01_KeywordsInJava.java
@@ -0,0 +1,37 @@
+package Unit_1;
+
+public class P2_Task_01_KeywordsInJava {
+		public static void main(String[] args) {
+//		Variable name
+//		Syntax error on token "double", invalid VariableDeclaratorId
+		int double = 12;
+		
+//		Function name 
+		int new(){
+			System.out.println("Function named as new");
+			return 0;
+		}
+		
+//		Class name [Written below]
+		
+//		Object name 
+		ABC int = new ABC();
+		int.display();
+		
+	}
+	class int () {
+		void display()
+		{
+			System.out.println("Hello");
+		}
+	}
+	class ABC() {
+		void display() {
+			void display()
+			{
+				System.out.println("Hello");
+			}
+		}
+	}
+	}
+}
