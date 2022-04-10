cc00238edecd48fbda01b479f0c6cb72701d832d
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-04-10 18:02:38 +0530

    Done

--- JavaPrograms/src/Unit_1/P6_Task_03_BufferClassInJava.java
+++ JavaPrograms/src/Unit_1/P6_Task_03_BufferClassInJava.java
@@ -13,7 +13,7 @@ public class P6_Task_03_BufferClassInJava {
 		
 		StringBuffer sb = new StringBuffer("Hello");
 		sb.append("Java");
-		System.out.println(sb.charAt[0]);
+//		System.out.println(sb.charAt[0]);
 		
 //		StringBuffer class append() method, now original string is changed
 		System.out.println(sb);
@@ -25,7 +25,7 @@ public class P6_Task_03_BufferClassInJava {
 
 //		StringBuffer replace() Method
 		StringBuffer sb3 = new StringBuffer("Hello");
-		sb3.repalce(1,3,"Java");   
+		sb3.replace(1,3,"Java");   
 		System.out.println(sb3);  //Print HJAvalo
 		
 //		StringBuffer delete() Method
