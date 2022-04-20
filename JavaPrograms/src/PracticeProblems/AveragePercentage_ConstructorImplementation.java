b6bf9fc17635c9b1bc0384318120143c53796137
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-04-20 10:16:15 +0530

    PP

--- JavaPrograms/src/PracticeProblems/AveragePercentage_ConstructorImplementation.java
+++ JavaPrograms/src/PracticeProblems/AveragePercentage_ConstructorImplementation.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class AveragePercentage_ConstructorImplementation {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		for(int i=1;i<=6;i++) {
-			System.out.println("Enter details of "+i+"st Student");
+		Student[] obj = new Student[6];
+		for(int i=0;i<6;i++) {
+			System.out.println("Enter details of "+(i+1)+"st Student");
 			System.out.print("Enter Name : ");
 			String name = in.next();
 			System.out.print("Enter Age : ");
 			byte age = in.nextByte();
 			System.out.print("Enter Percentage: ");
 			int perc = in.nextInt();
-			Student obj = new Student(name,age,"A",perc);
+			obj[i] = new Student(name,age,"A",perc);
 		}
 		System.out.println("Average of all 6 students is "+(float)Student.avg/6);
 		in.close();
