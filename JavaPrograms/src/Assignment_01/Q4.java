b97eb945ddc0468f8d1f375f38b2c8900cddda62
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-04-23 21:35:19 +0530

    Done

---
+++ JavaPrograms/src/Assignment_01/Q4.java
@@ -0,0 +1,44 @@
+/*
+  Create a multilevel inheritance hierarchy from ClassOne, ClassTwo, ClassThree classes.
+  ClassOne contains one, single parameterized constructor ClassTwo contains only a default constructor ClassThree 
+  contains one, single parameterized constructor and a default constructor.
+  Create an object of top child class and make sure the constructor execution will take place according to respective 
+  Multilevel inheritance hierarchy.
+  
+	ClassThree obj = new ClassThree(1);
+	ClassThree obj = new ClassThree()
+ 
+ */
+
+package Assignment_01;
+
+public class Q4 {
+	public static void main(String[] args) {
+		ClassThree obj = new ClassThree(1);
+		System.out.println();
+		ClassThree obj1 = new ClassThree();
+	}
+}
+
+class ClassOne{
+	ClassOne(int a){
+    	System.out.println("Class One => Paramterized Constructor");
+	}
+}
+
+class ClassTwo extends ClassOne{
+	ClassTwo(){
+		super(1);
+		System.out.println("Class Two => Default Constructor");
+	}
+}
+
+class ClassThree extends ClassTwo{
+	ClassThree(){
+		System.out.println("Class Three => Default Constructor");
+	}
+
+	ClassThree(int a){
+		System.out.println("Class Three => Paramterized Constructor");
+	}
+}
