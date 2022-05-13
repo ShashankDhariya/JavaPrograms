65cdae342e0ecb23564f3a1fbc0bb2f1cd988942
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-05-13 16:21:53 +0530

    Done

--- JavaPrograms/src/Unit_3/P14_ExceptionHandling.java
+++ JavaPrograms/src/Unit_3/P14_ExceptionHandling.java
@@ -3,6 +3,8 @@ package Unit_3;
 // Error can't be handled in any of the case while exception can be handled.
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class P14_ExceptionHandling {
 	public static void main(String[] args) {
 		
@@ -15,12 +17,13 @@ public class P14_ExceptionHandling {
 //		obj.stackOverFlow(10);				// Stack overflow is not an exception, it's a error
 		obj.indexOutOfBoundException();
 		obj.nullPointerException();
-		obj.useOfThrow();
+//		obj.useOfThrow(17);
 		obj.useOfThrows_And_Finally();
 	}
 }
 
 class exception{
+	
 	void basicException() {
 		
 //		Compiler can't catch this exception at compile time (Divide by zero(0) exception) 
@@ -28,6 +31,7 @@ class exception{
 		int a = 100/b;
 		System.out.println(a);
 	}
+	
 	void handleException() {
 		try {
 //			Sensitive code
@@ -39,6 +43,7 @@ class exception{
 		}
 		System.out.println("Done");
 	}
+	
 	void multipleCatch() {
 		try {
 //			Sensitive code
@@ -53,6 +58,7 @@ class exception{
 			System.out.println(e.getMessage()+" exception handled");
 		}
 	}
+	
 	void inputMismatchException() {
 		try{
 			Scanner in = new Scanner(System.in);
@@ -60,11 +66,12 @@ class exception{
 			int a = in.nextInt();
 			System.out.println("Entered number : "+a);
 			in.close();
-		}catch(inputMisMatchException e) {
-			System.out.println(e.getMessage);
+		}catch(InputMismatchException e) {
+			System.out.println("Input Mismatch Exception");
 		}
 		System.out.println("Done");
 	}
+	
 	void stackOverFlow(int i) {
 		try{
 			while(i>0) {
@@ -76,6 +83,7 @@ class exception{
 		}
 		System.out.println("Done");
 	}
+	
 	void indexOutOfBoundException() {
 		try{
 			int[] myNumbers = {1, 2, 3};
@@ -85,13 +93,35 @@ class exception{
 		}
 		System.out.println("Done");
 	}
+	
 	void nullPointerException() {
-		
+		try{
+			String s = null;
+			System.out.println(s.length());
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Done");
 	}
-	void useOfThrow() {
-		
+	
+	void useOfThrow(int age) throws Exception{
+		if(age<18)
+			throw new Exception();
+		else 
+			System.out.println("You are eligible");
 	}
+	
 	void useOfThrows_And_Finally() {
+		try {
+			useOfThrow(17);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		finally {
+			System.out.println("Exception handled succesfully");
+		}
 		
 	}
 }
