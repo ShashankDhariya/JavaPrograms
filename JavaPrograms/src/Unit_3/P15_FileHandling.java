79a0856bd8b690b63e97845b6630bd3c4670f619
Shashank Dhariya <Shashank Dhariya@LAPTOP-GU46GA8D>
2022-05-17 11:51:41 +0530

    Done

---
+++ JavaPrograms/src/Unit_3/P15_FileHandling.java
@@ -0,0 +1,22 @@
+package Unit_3;
+
+public class P15_FileHandling {
+	public static void main(String[] args) {
+		FileHandling obj = new FileHandling();
+		obj.takeInputFromConsole();
+		obj.saveOutputIntoAFile();
+		obj.createALogFile();
+	}
+}
+
+class FileHandling{
+	void takeInputFromConsole() {
+		
+	}
+	void saveOutputIntoAFile() {
+		
+	}
+	void createALogFile() {
+		
+	}
+}
---
+++ JavaPrograms/src/Unit_3/P16_MultiThreading.java
@@ -0,0 +1,31 @@
+package Unit_3;
+
+public class P16_MultiThreading {
+	public static void main(String[] args) {
+		MultiThreading obj = new MultiThreading();
+		obj.threadConcept();
+		obj.threadSynchronization();
+		obj.interThreadCommunication();
+		obj.producerConsumerProblems();
+		obj.WaitAndNotify();
+	}
+}
+
+class MultiThreading
+{
+	void threadConcept() {
+		
+	}
+	void threadSynchronization() {
+		
+	}
+	void interThreadCommunication() {
+		
+	}
+	void producerConsumerProblems() {
+		
+	}
+	void WaitAndNotify() {
+		
+	}
+}
